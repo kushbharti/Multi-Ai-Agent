44f88c1e4ff286523dcc32b38d181f2ca3683d08
kushbharti <try.kushbharti@gmail.com>
2026-07-19 21:52:50 +0530

    Agent Service Setup

--- backend/services/chat/models/message.model.js
+++ backend/services/chat/models/message.model.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema(
   {
     conversationId: {
-      type: mongoose.Schema.Typea.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
       ref: "Conversation",
     },
     role: {
