3b4d37cb8c98c6b52bc9d514bc394386777c4a67
kushbharti <try.kushbharti@gmail.com>
2026-07-27 23:13:45 +0530

    creating redux tool kit

--- frontend/src/components/ChatArea.jsx
+++ frontend/src/components/ChatArea.jsx
@@ -1,13 +1,32 @@
+import { useEffect } from "react";
 import ChatInput from "./ChatInput";
 import MessageList from "./MessageList";
 import Nav from "./Nav";
+import { useSelector } from "react-redux";
+import getMessages from "../features/getMessages";
+import { setMessages } from "../redux/messageSlice";
+import { useDispatch } from "react-redux";
 
 function ChatArea() {
-  return <div className="flex-1 flex flex-col">
-    <Nav />
-    <MessageList/>
-    <ChatInput/>
-  </div>;
+  const dispatch = useDispatch();
+  const { selectedConversation } = useSelector((state) => state.conversation);
+  useEffect(() => {
+    const getMsg = async () => {
+      if (selectedConversation) {
+        const data = await getMessages(selectedConversation?._id);
+        dispatch(setMessages(data));
+      }
+    };
+    getMsg();
+  }, [selectedConversation]);
+
+  return (
+    <div className="flex-1 flex flex-col">
+      <Nav />
+      <MessageList />
+      <ChatInput />
+    </div>
+  );
 }
 
 export default ChatArea;
